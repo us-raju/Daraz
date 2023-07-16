af2c290630d1adafa4d9390da91bcc3815e6fcc6
us-raju <usraju900@gmail.com>
2023-07-16 17:33:30 +0600

    navbar added

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(function () {
+//    Home banner slider js start  
+    $(".home_banner_slider").slick({
+        prevArrow:false,
+        nextArrow:false,
+    });
+//    Home banner slider js end  
+});
