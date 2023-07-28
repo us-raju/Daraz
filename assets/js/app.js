ff06c2a7b78be61c303d8634ce8019896327bf99
us-raju <usraju900@gmail.com>
2023-07-29 02:58:20 +0600

    problem solve

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,31 @@
 $(function () {
-//    Home banner slider js start  
-    $(".home_banner_slider").slick({
-        prevArrow:false,
-        nextArrow:false,
-    });
-//    Home banner slider js end  
+  //    Home banner slider js start
+  $(".home_banner_slider").slick({
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+    dotsClass: "home_banner_dots",
+  });
+  //    Home banner slider js end
+
+  // tooltip start
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+  // tooltip end
+
+  // the final countdown start
+   $("#clock").countdown("2030/10/10", function (event) {
+     var $this = $(this).html(
+       event.strftime(
+         "" +
+           '<div class="deals_timer_param"><span>%D</span></div>' +
+           '<span class="deals_timer_clone">:</span>' +
+           '<div class="deals_timer_param"><span>%H</span></div>' +
+           '<span class="deals_timer_clone">:</span>' +
+           '<div class="deals_timer_param"><span>%S</span></div>'
+       )
+     );
+   });
+  // the final countdown end
 });
